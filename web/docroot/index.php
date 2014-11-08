871dc74cb1507ea37b9fdd4f0d1fb4ae130fa246
jplsek <jeremyplsek@gmail.com>
2014-11-08 18:17:28 -0500

    Added an interface to work from.

--- web/docroot/index.php
+++ web/docroot/index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <link href="/css/bootstrap.min.css" rel="stylesheet"/>
-    <link rel="stylesheet" href="css/styles.css">
+    <link href="/css/style.css" rel="stylesheet">
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -14,34 +14,87 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-inverse" role="navigation">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Project name</a>
-            </div>
-            <div id="navbar" class="collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                </ul>
-            </div><!--/.nav-collapse -->
+    <div id="playlist" class="">
+        <div id="playlist-controls">
+            <span class="glyphicon glyphicon-repeat playlist-control playlist-control-backward"></span>
+            <span class="glyphicon glyphicon-random playlist-control playlist-control-backward"></span>
+            <br>
+            <span class="glyphicon glyphicon-step-backward playlist-control playlist-control-backward"></span>
+            <span class="glyphicon glyphicon-step-forward playlist-control playlist-control-forward"></span>
         </div>
-    </nav>
+        <div id="playlist-list">
+            <ul class="nav nav-pills nav-stacked" role="tablist">
+                <li class="active"><a href="#">Playlist1</a></li>
+                <li><a href="#">Playlist2</a></li>
+                <li><a href="#">Playlist3</a></li>
+                <li><a href="#">Playlist4</a></li>
+            </ul>
+        </div>
 
-	<div class="container">
+    </div>
 
-	<h1>Unique name goes here?</h1>
+    <div id="song-content" class="">
 
-	</div><!-- /.container -->
+        <div class="container-fluid">
+            <div class="row">
+                <div id="song-info" class="col-sm-6">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <img class="img-responsive song-img" src="/img/dpa-tmp.jpg" alt="Album Cover"> <!-- Obviously a placeholder. -->
+                        </div>
+                        <div class="song-info-text">
+                            <p>Song Name</p>
+                            <p>Artist Name</p>
+                            <p>Album Name</p>
+                        </div>
+                    </div>
+                </div>
+                <div id="song-controls" class="col-sm-6">
+                    <span class="glyphicon glyphicon-step-backward song-control song-control-backward"></span>
+                    <span class="glyphicon glyphicon-pause song-control song-control-pause"></span>
+                    <span class="glyphicon glyphicon-step-forward song-control song-control-forward"></span>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-	<script src="js/jquery-2.1.1.min.js"></script>
-	<script src="js/js.js"></script>
+    </div>
+
+    <div id="playlist-content" class="">
+
+        <div id="playlist-song-list">
+            <ul class="nav nav-pills nav-stacked" role="tablist">
+                <li class="playlist-sort">
+                    <span class="song-play"></span>
+                    <span class="song-name">Song</span>
+                    <span class="song-artist">Artist</span>
+                    <span class="song-album">Album</span>
+                    <span class="song-time">Time</span>
+                </li>
+                <li>
+                    <span class="glyphicon glyphicon-play song-play"></span>
+                    <span class="song-name">Song2</span>
+                    <span class="song-artist">Artist name</span>
+                    <span class="song-album">Album name</span>
+                    <span class="song-time">time</span>
+                </li>
+                <li>
+                    <span class="glyphicon glyphicon-play song-play"></span>
+                    <span class="song-name">Song asdasdsadadas ad sada</span>
+                    <span class="song-artist">Artist name</span>
+                    <span class="song-album">Album name</span>
+                    <span class="song-time">time</span>
+                </li>
+            </div>
+        </div>
+
+    </div>
+
+    <script src="js/jquery-2.1.1.min.js"></script>
+    <script src="js/js.js"></script>
 
 </body>
 </html>
